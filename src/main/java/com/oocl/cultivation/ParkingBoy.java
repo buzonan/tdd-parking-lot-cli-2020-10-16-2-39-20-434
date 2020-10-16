99325b5fcc54abd1f8613baa468b45314d31781d
Andrey Buzon <andrey.bryan.buzon@oocl.com>
2020-10-16 16:42:30 +0800

    [ParkingLot][andrey]refactor: handle park and fetch method in ParkingLot Class instead of inside ParkingBoy Class and create default value for Parking Lot with capacity of 10 with no parked cars.

--- src/main/java/com/oocl/cultivation/ParkingBoy.java
+++ src/main/java/com/oocl/cultivation/ParkingBoy.java
@@ -1,34 +1,20 @@
 package com.oocl.cultivation;
 
-import java.util.HashMap;
-import java.util.Map;
-
 public class ParkingBoy{
-    private final Map<ParkingTicket, Car> ticketCarMap = new HashMap<>();
-    public int capacity;
-    public int carsParked;
+    ParkingLot parkingLot;
 
     public ParkingBoy(ParkingLot parkingLot) {
-        this.carsParked = parkingLot.getCarsParked();
-        this.capacity = parkingLot.getCapacity();
+        this.parkingLot = parkingLot;
     }
 
     public ParkingTicket park(Car car) {
-        if(!isParkingLotFull()){
-            ParkingTicket parkingTicket = new ParkingTicket();
-            ticketCarMap.put(parkingTicket, car);
-            return parkingTicket;
+        if(!parkingLot.isParkingLotFull()){
+            return parkingLot.park(car);
         }
         return null;
     }
 
-    private boolean isParkingLotFull() {
-        return capacity == carsParked;
-    }
-
     public Car fetch(ParkingTicket parkingTicket) {
-        Car carFromTicket = ticketCarMap.get(parkingTicket);
-        ticketCarMap.remove(parkingTicket);
-        return carFromTicket;
+        return parkingLot.fetch(parkingTicket);
     }
 }
--- src/main/java/com/oocl/cultivation/ParkingLot.java
+++ src/main/java/com/oocl/cultivation/ParkingLot.java
@@ -1,19 +1,36 @@
 package com.oocl.cultivation;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class ParkingLot {
+    private Map<ParkingTicket, Car> ticketCarMap = new HashMap<>();
     private int capacity;
     private int carsParked;
+
+    public ParkingLot() {
+        this.capacity = 10;
+        this.carsParked = 0;
+    }
 
     public ParkingLot(int capacity, int carsParked) {
         this.capacity = capacity;
         this.carsParked = carsParked;
     }
 
-    public int getCapacity() {
-        return capacity;
+    public boolean isParkingLotFull() {
+        return capacity == carsParked;
     }
 
-    public int getCarsParked() {
-        return carsParked;
+    public ParkingTicket park(Car car) {
+        ParkingTicket parkingTicket = new ParkingTicket();
+        ticketCarMap.put(parkingTicket, car);
+        return parkingTicket;
+    }
+
+    public Car fetch(ParkingTicket parkingTicket) {
+        Car carFromTicket = ticketCarMap.get(parkingTicket);
+        ticketCarMap.remove(parkingTicket);
+        return carFromTicket;
     }
 }
