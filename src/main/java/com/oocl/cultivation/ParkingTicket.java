1ff671fc28924d76166881b67c78567dd9fcad9c
Andrey Buzon <andrey.bryan.buzon@oocl.com>
2020-10-16 14:04:45 +0800

    [ParkingLot][andrey]feature: Create ticket when parking boy park the car

---
+++ src/main/java/com/oocl/cultivation/ParkingTicket.java
@@ -0,0 +1,6 @@
+package com.oocl.cultivation;
+
+public class ParkingTicket {
+    public ParkingTicket(Car car) {
+    }
+}
--- src/test/java/com/oocl/cultivation/ParkingBoyTest.java
+++ src/test/java/com/oocl/cultivation/ParkingBoyTest.java
@@ -18,6 +18,5 @@ class ParkingBoyTest {
 
         //THEN
         assertNotNull(parkingTicket);
-
     }
 }
