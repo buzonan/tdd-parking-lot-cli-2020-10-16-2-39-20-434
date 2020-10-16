d34e406f85da0a608db10b809fd6208d9ad2ed78
Andrey Buzon <andrey.bryan.buzon@oocl.com>
2020-10-16 13:59:30 +0800

    [ParkingLot][andrey]test: Create Unit Test Case to return parking ticket given parking boy park the car.

--- src/test/java/com/oocl/cultivation/ParkingBoyTest.java
+++ src/test/java/com/oocl/cultivation/ParkingBoyTest.java
@@ -2,9 +2,22 @@ package com.oocl.cultivation;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.concurrent.locks.LockSupport;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
 class ParkingBoyTest {
     @Test
-    void write_your_first_test() {
+    void should_return_parking_ticket_when_parking_given_parking_boy_park() {
+        //GIVEN
+        Car car = new Car();
+        ParkingBoy parkingBoy = new ParkingBoy(new ParkingLot());
+
+        //WHEN
+        ParkingTicket parkingTicket = parkingBoy.park(car);
+
+        //THEN
+        assertNotNull(parkingTicket);
 
     }
 }
