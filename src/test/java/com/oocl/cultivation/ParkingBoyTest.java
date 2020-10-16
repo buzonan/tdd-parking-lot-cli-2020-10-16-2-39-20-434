84812415b5fa501af158df55fbba4009ff21bb90
Andrey Buzon <andrey.bryan.buzon@oocl.com>
2020-10-16 14:08:31 +0800

    [ParkingLot][andrey]test: Create Unit Test Case to return correct car given correct ticket.

--- src/test/java/com/oocl/cultivation/ParkingBoyTest.java
+++ src/test/java/com/oocl/cultivation/ParkingBoyTest.java
@@ -5,8 +5,10 @@ import org.junit.jupiter.api.Test;
 import java.util.concurrent.locks.LockSupport;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertSame;
 
 class ParkingBoyTest {
+
     @Test
     void should_return_parking_ticket_when_parking_given_parking_boy_park() {
         //GIVEN
@@ -18,5 +20,19 @@ class ParkingBoyTest {
 
         //THEN
         assertNotNull(parkingTicket);
+    }
+
+    @Test
+    void should_return_correct_car_when_fetch_given_correct_ticket() {
+        //GIVEN
+        Car car = new Car();
+        ParkingBoy parkingBoy = new ParkingBoy(new ParkingLot());
+        ParkingTicket parkingTicket = parkingBoy.park(car);
+
+        //WHEN
+        Car fetchedCar = parkingBoy.fetch(parkingTicket);
+
+        //THEN
+        assertSame(car, fetchedCar);
     }
 }
